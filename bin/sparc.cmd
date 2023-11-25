895c9b4236162a43db952ce6cecf5ba3d44ca8ba
Shawn Presser <shawnpresser@gmail.com>
2023-11-25 00:39:08 -0600

    Update bin/sparc.cmd

--- bin/sparc.cmd
+++ bin/sparc.cmd
@@ -1,10 +1,14 @@
 @echo off
 setlocal
 
-set home=%~dp0
-
-cd "%home%"
+set "dir=%cd%"
+cd "%~dp0"
 cd ..
+set "home=%cd%"
+cd "%dir%"
 
-set DEV=1
-racket -t ac.scm %*
+endlocal
+
+REM set DEV=1
+REM
+IF "%1"=="" ( CALL "%home%\bin\arc.cmd" "%home%\news.arc" ) ELSE ( CALL "%home%\bin\arc.cmd" %* )
