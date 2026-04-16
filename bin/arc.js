c62cae05792980f71c971c8f4af5f81fa16f7073
Shawn Presser <shawnpresser@gmail.com>
2026-04-16 05:20:53 -0500

    Use JS wrappers for bin entries, fix npm 11 bin validation

---
+++ bin/arc.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+'use strict';
+const { spawnSync } = require('child_process');
+const path = require('path');
+const script = process.platform === 'win32'
+  ? path.join(__dirname, 'arc.cmd')
+  : path.join(__dirname, 'arc');
+const result = spawnSync(script, process.argv.slice(2), { stdio: 'inherit' });
+process.exit(result.status ?? 1);
